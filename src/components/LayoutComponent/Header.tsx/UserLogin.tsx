9f274ea388b914ae929e03ecaf35fd724c63bce1
Nguyen Ngoc Khanh <84921391+hellokhanhne@users.noreply.github.com>
2022-08-04 02:48:22 +0700

    m

--- src/components/LayoutComponent/Header.tsx/UserLogin.tsx
+++ src/components/LayoutComponent/Header.tsx/UserLogin.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const UserLogin = ({
   avatar = '',
@@ -12,8 +13,17 @@ const UserLogin = ({
     window.location.href = '/login';
   };
 
+  const navigate = useNavigate();
+
   return (
-    <div className='d-flex user-logined pointer align-items-center'>
+    <div
+      className='d-flex user-logined pointer align-items-center'
+      onClick={() =>
+        navigate('/manage/profile', {
+          replace: true,
+        })
+      }
+    >
       <img src={avatar} alt='' />
       <span className='text-white'>{username}</span>
       <i className='fas fa-sign-out-alt mr-1' onClick={logout}></i>
